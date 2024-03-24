71bbeb06e5a569657df4c6cac10674311ebde4d7
Ali Eren Karadeniz <151842601+aerenkrdnz@users.noreply.github.com>
2024-03-24 23:30:57 +0300

    Add files via upload

---
+++ MarketoExpress/MarketoExpress.WebUI/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MarketoExpress.WebUI.Areas.Admin.Controllers
+{
+	public class DashboardController : Controller
+	{
+		[Area("Admin")]
+		[Authorize(Roles = "Admin")]
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
