c088ee05f1f5ab58c8a7263fb2ac866ae045e4f7
Ali Eren Karadeniz <151842601+aerenkrdnz@users.noreply.github.com>
2024-03-27 13:47:26 +0300

    Add files via upload

---
+++ MarketoExpress/MarketoExpress/MarketoExpress.Business/Managers/UserManager.cs
@@ -0,0 +1,78 @@
+﻿using MarketoExpress.Business.Dtos;
+using MarketoExpress.Business.Services;
+using MarketoExpress.Business.Types;
+using MarketoExpress.Data.Entities;
+using MarketoExpress.Data.Enums;
+using MarketoExpress.Data.Repositories;
+using Microsoft.AspNetCore.DataProtection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketoExpress.Business.Managers
+{
+	public class UserManager : IUserService
+	{
+		private readonly IRepository<UserEntity> _userRepository;
+		private readonly IDataProtector _dataProtector;
+        public UserManager(IRepository<UserEntity> userRepository, IDataProtectionProvider dataProtectionProvider)
+        {
+            _userRepository = userRepository;
+			_dataProtector = dataProtectionProvider.CreateProtector("security");
+        }
+        public ServiceMessage AddUser(AddUserDto addUserDto)
+		{
+			var hasMail = _userRepository.GetAll(x => x.Email.ToLower() == addUserDto.Email.ToLower()).ToList();
+			if (hasMail.Any())
+			{
+				return new ServiceMessage()
+				{
+					IsSucceed = false,
+					Message = "Bu Eposta adresli bir kullanıcı zaten mevcut."
+				};
+			}
+			var entity = new UserEntity()
+			{
+				Email = addUserDto.Email,
+				FirstName = addUserDto.FirstName,
+				LastName = addUserDto.LastName,
+				Password = _dataProtector.Protect(addUserDto.Password),
+				UserType = UserTypeEnum.User
+			};
+			_userRepository.Add(entity);
+
+			return new ServiceMessage()
+			{
+				IsSucceed = true,
+				
+			};
+		}
+
+		public UserInfoDto LoginUser(LoginDto loginDto)
+		{
+			var userEntity = _userRepository.Get(x => x.Email == loginDto.Email);
+			if(userEntity is null)
+			{
+				return null;
+			}
+			var rawPassword = _dataProtector.Unprotect(userEntity.Password);
+			if(rawPassword == loginDto.Password)
+			{
+				return new UserInfoDto()
+				{
+					Id = userEntity.Id,
+					Email = userEntity.Email,
+					FirstName = userEntity.FirstName,
+					LastName = userEntity.LastName,
+					UserType = userEntity.UserType,
+				};
+			}
+			else
+			{
+				return null;
+			}
+		}
+	}
+}
---
+++ MarketoExpress/MarketoExpress/MarketoExpress.WebUI/Program.cs
@@ -0,0 +1,34 @@
+using MarketoExpress.Business.Managers;
+using MarketoExpress.Business.Services;
+using MarketoExpress.Data.Context;
+using MarketoExpress.Data.Repositories;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<MarketoExpressContext>(options => options.UseSqlServer(connectionString));
+builder.Services.AddScoped(typeof(IRepository<>), typeof(SqlRepository<>));
+builder.Services.AddScoped<IUserService,UserManager>();
+builder.Services.AddScoped<ICategoryService, CategoryManager>();
+builder.Services.AddScoped<IProductService, ProductManager>();
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options =>
+{
+	options.LoginPath = new PathString("/");
+	options.LogoutPath = new PathString("/");
+	options.AccessDeniedPath = new PathString("/");
+
+});
+var app = builder.Build();
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseStaticFiles();
+app.MapControllerRoute(
+	name: "areas",
+	pattern: "{area:exists}/{Controller=Dashboard}/{Action=Index}/{id?}"
+	);
+
+app.MapDefaultControllerRoute();
+
+app.Run();
